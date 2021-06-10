f20f74f6ca4e55a58d2739d79bc2d0fdc49b8edd
Luis Chang <luis.chang@outlook.com>
2021-06-09 21:19:19 -0500

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <div id="Login">
+      <h1>Inicio de Sesión</h1>
+      <v-text-field v-model="username" name="username" label="Usuario">
+      </v-text-field>
+      <v-text-field v-model="password" name="password" label="Contraseña">
+      </v-text-field>
+      <v-btn primary large block v-on:click="login">Login</v-btn>
+      <br />
+      <h3 v-show="errored" class="hideElement">
+        El usuario y/o la contraseña es incorrecto. Intente nuevamente.
+      </h3>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      errored: false,
+    };
+  },
+  methods: {
+    login: function () {
+      console.log("Este es un click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  width: 500px;
+  border: 1px solid #cccccc;
+  background-color: #cccccc;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
+</style>
